5849f05bc156002e849318a27d8f156680e0c2b0
Kishore T J <kishoretj2908@gmail.com>
2025-02-16 16:37:28 +0530

    This commit is from gitsftuffs

--- src/java/Encapsualtion.java
+++ src/java/Encapsualtion.java
@@ -4,6 +4,8 @@ public class Encapsualtion {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("Auto new i.e Git Stuff");
+		System.out.println("---------");
 	}
 
 }
